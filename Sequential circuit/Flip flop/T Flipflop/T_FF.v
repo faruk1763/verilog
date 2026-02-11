79ad79d8107bc1167f44cca5402f788f54476c92
faruk1763 <mohamedfaruk.25.01@gmail.com>
2026-02-11 11:37:07 +0530

    Implement T Flip-Flop module in Verilog

--- Sequential circuit/Flip flop/T Flipflop/T_FF.v
+++ Sequential circuit/Flip flop/T Flipflop/T_FF.v
@@ -1,1 +1,13 @@
-
+module tff(input clk,rst,t,output reg q,qbar);
+  always@(posedge clk or posedge rst)
+    begin
+    
+    if(rst)
+      q<=1'b0;
+      else if (t)
+      q<=~q;
+    else
+      q<=q;
+      end
+       assign qbar=q;
+      endmodule
