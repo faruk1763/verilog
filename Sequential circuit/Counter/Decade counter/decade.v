43887e9594e1337ccd04690a1777144b727a8895
faruk1763 <mohamedfaruk.25.01@gmail.com>
2026-02-21 12:19:04 +0530

    Add decade counter module for counting to 10

---
+++ Sequential circuit/Counter/Decade counter/decade.v
@@ -0,0 +1,12 @@
+module decade(input clk,rst,output [3:0]q);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        q<=4'b0000;
+      else if(q==4'b1001)
+       q<=4'b0000;
+      else
+        q<=q+1;
+    end
+endmodule
+        
