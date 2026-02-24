da953fbe71028bb15b93c35e380c49f22b1fc404
faruk1763 <mohamedfaruk.25.01@gmail.com>
2026-02-24 17:25:28 +0530

    Create pipo.v

---
+++ Sequential circuit/Register/PIPO/pipo.v
@@ -0,0 +1,8 @@
+module pipo#(parameter n=4)(input clk, rst, load,input [n-1:0] pin, output reg [n-1:0] pout);
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        pout <= 0;
+    else if (load)
+        pout <= pin;
+end
+endmodule
