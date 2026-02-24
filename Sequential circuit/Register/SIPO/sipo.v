6eceef7a50fca32f4f981f630c4d6887b95a8e4b
faruk1763 <mohamedfaruk.25.01@gmail.com>
2026-02-24 17:52:57 +0530

    Update sipo.v

--- Sequential circuit/Register/SIPO/sipo.v
+++ Sequential circuit/Register/SIPO/sipo.v
@@ -1,9 +1,9 @@
-module sipo#(parameter n=4)(input clk,rst,load,sin,output reg [n-1:0]q);
+module sipo#(parameter n=4)(input clk,rst,load,sin,output reg [n-1:0]pout);
  always @(posedge clk )
 begin
 if(rst)
- q<=1'b0;
+ pout<=0;
 else if(load)
- q<={sin,q[n-1:1]};
+ pout<={sin,pout[n-1:1]};
 end
 endmodule
